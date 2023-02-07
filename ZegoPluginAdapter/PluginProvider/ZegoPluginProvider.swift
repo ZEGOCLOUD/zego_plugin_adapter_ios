e7d7c51ffaac10e5a9bf3daab781efdbe02f1094
careyxi <careyxi@zego.im>
2023-02-07 14:11:53 +0800

    add plugin adapter.

---
+++ ZegoPluginAdapter/PluginProvider/ZegoPluginProvider.swift
@@ -0,0 +1,16 @@
+//
+//  ZegoPluginProvider.swift
+//  Pods-ZegoUIKitSDK
+//
+//  Created by zego on 2022/12/15.
+//
+
+import Foundation
+
+public protocol ZegoPluginProvider {
+    func getPlugin() -> ZegoPluginProtocol?
+}
+
+public struct ZegoSignalingProvider {
+
+}
